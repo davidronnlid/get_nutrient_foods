13ebc2ea27f9a886af6ca3e6c1c07c0f6d366fa0
David Rönnlid - Nootralize <dr@nootralize.com>
2021-08-05 10:33:39 +0200

    About project card WIP almost done

--- src/aboutProjectCard.js
+++ src/aboutProjectCard.js
@@ -1,16 +1,42 @@
+import ConfidentSmile from "./ConfidentSmile.jpg";
+
 const AboutProjectCard = () => {
   return (
-    <div className="buttons">
-      <button className="button is-primary">Primary</button>
-      <button className="button is-link">Link</button>
-      <button className="button is-info">Info</button>
-      <button className="button is-success">Success</button>
-      <button className="button is-warning">Warning</button>
-      <button className="button is-danger">Danger</button>
-      <button className="button is-black">Black</button>
-      <button className="button is-white">White</button>
-      <button className="button is-dark">Dark</button>
-      <button className="button is-light">Light</button>
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title title is-1">About</p>
+        <button className="card-header-icon" aria-label="more options">
+          <span className="icon">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </header>
+      <div className="card-image">
+        {/* Change size of card (is-XYZ) and commit todays work to GitHub */}
+        <figure className="image">
+          <img src={ConfidentSmile} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="content">
+          The code for this project was written by David Rönnlid. No help except
+          Google, Stack Overflow, blogs, etc. was used.
+        </div>
+      </div>
+      <footer className="card-footer">
+        <a
+          href="https://github.com/davidronnlid/get_nutrient_foods"
+          className="card-footer-item"
+        >
+          Project Github page
+        </a>
+        <a
+          href="https://www.linkedin.com/in/davidronnlid/"
+          className="card-footer-item"
+        >
+          David Rönnlid on LinkedIn
+        </a>
+      </footer>
     </div>
   );
 };
